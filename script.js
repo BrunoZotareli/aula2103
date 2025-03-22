9ad8255dd2a03223fe5901ee17ecd7f9dc62f285
BrunoZotareli <zotarelibruno@gmail.com>
2025-03-21 22:19:42 -0300

    aula

---
+++ script.js
@@ -0,0 +1,14 @@
+function ligar(){
+let lampada=document.getElementById('lampada')
+
+lampada.src = 'imagens/macacod.jpg';
+
+}
+
+
+
+function desligar(){
+
+    let lampada=document.getElementById('lampada').src = 'imagens/sem.png';
+
+}
